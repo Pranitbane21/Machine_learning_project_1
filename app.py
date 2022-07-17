c724d807a90b9e34c1f614e0b7407458c8b7bb8b
Pranitbane21 <pranitbane8@gmail.com>
2022-07-18 00:40:02 +0530

    File structure updated

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "CI CD pipeline as been established."
+    try:
+        raise Exception("we are testing exception")
+    except Exception as e:
+        housing=HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
+    return "Exception testing"
 
 if __name__=="__main__":
     app.run(debug=True)
